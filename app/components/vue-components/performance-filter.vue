b14196935a116297b6a90fd7c3fc3bba8113879d
Andrew Lawendy <andrew.lawendy@outlook.com>
2022-05-11 12:33:21 +0200

    feat: :sparkles: add date input to filter the chart

---
+++ app/components/vue-components/performance-filter.vue
@@ -0,0 +1,38 @@
+<template>
+  <fragment>
+    <div>
+      <label>Start Date</label>
+      <input type="date" v-model="startDate" @input="getData" />
+    </div>
+
+    <div>
+      <label>End Date</label>
+      <input type="date" v-model="endDate" @input="getData" />
+    </div>
+  </fragment>
+</template>
+
+<script>
+import { Fragment } from 'vue-fragment';
+import moment from 'moment';
+
+export default {
+  name: 'PerformanceFilter',
+  components: { Fragment },
+
+  data() {
+    return {
+      startDate: moment().format('YYYY-MM-DD'),
+      endDate: moment().format('YYYY-MM-DD'),
+    };
+  },
+
+  methods: {
+    getData() {
+      console.log(this.startDate, '<---->', this.endDate);
+    },
+  },
+};
+</script>
+
+<style></style>
--- app/pages/performance-page.vue
+++ app/pages/performance-page.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="c-dashboard">
     <div class="c-filters__container">
-      [Place filter component here]
+      <performance-filter-component />
     </div>
     <div class="c-dashboard__header">
       <performance-chart-component />
@@ -10,11 +10,13 @@
 </template>
 
 <script>
-import PerformanceChartComponent from "../components/vue-components/performance-chart.vue";
+import PerformanceChartComponent from '../components/vue-components/performance-chart.vue';
+import PerformanceFilterComponent from '../components/vue-components/performance-filter.vue';
 export default {
-  name: "AboutPageComponent",
+  name: 'AboutPageComponent',
   components: {
     PerformanceChartComponent,
+    PerformanceFilterComponent,
   },
 };
 </script>
